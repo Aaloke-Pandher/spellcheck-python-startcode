acea54c8a6312f4d456aebd6177735e127594be0
Aaloke-Pandher <98190131+Aaloke-Pandher@users.noreply.github.com>
2023-10-31 12:49:37 -0600

    Update spellcheck.py

--- spellcheck.py
+++ spellcheck.py
@@ -5,6 +5,8 @@
 
 import re  # Needed for splitting text with a regular expression
 
+import time
+import math
 
 def main():
     # Load data files into lists
@@ -13,7 +15,63 @@ def main():
 
     # Print first 50 values of each list to verify contents
     print(dictionary[0:50])
-    print(aliceWords[0:50])
+    print(aliceWords[0:50]) 
+    loop = True 
+    while loop: 
+        # Print Menu
+        print("\nMain Menu")
+        print("1. Spell Check a Word (Linear Search)") 
+        print("2. Spell Check a Word (Binary Search)") 
+        print("3. Spell Check Alice in Wonderland (Linear Search)") 
+        print("4. Spell Check Alice in Wonderland (Binary Search)")   
+        print("5. Exit") 
+
+        # Get Menu Selection from User
+        selection = input("Enter Selection (1-5): ") 
+
+        if selection == "1": 
+            startTime = time.time() 
+            wordInput = input("Enter a word: ") 
+            if linearSearch(dictionary, wordInput) == -1:
+                endTime = time.time() 
+                timeElapsed = endTime - startTime
+                print("Word could not be found. (" + str(timeElapsed) + " seconds)") 
+            else:
+                endTime = time.time() 
+                timeElapsed = endTime - startTime 
+                print("Word was found at position " + str(linearSearch(dictionary, wordInput)) + ". (" + str(timeElapsed) + " seconds)")
+        elif selection == "2": 
+            startTime = time.time()
+            word = input("Enter a word: ") 
+            if binarySearch(dictionary, word) == -1: 
+                endTime = time.time() 
+                timeElapsed = endTime - startTime
+                print("Word could not be found. (" + str(timeElapsed) + " seconds)")
+            else:
+                endTime = time.time() 
+                timeElapsed = endTime - startTime 
+                print("Word was found at position " + str(binarySearch(dictionary, word)) + ". (" + str(timeElapsed) + " seconds)") 
+        elif selection == "3": 
+            notWords = 0
+            startTime = time.time()
+            for i in range(len(aliceWords)): 
+                if linearSearch(dictionary, aliceWords[i].lower()) == -1: 
+                    endTime = time.time() 
+                    timeElapsed = endTime - startTime 
+                    notWords += 1 
+            print("Number of words not found in the dictionary: " + str(notWords) + " (" + str(timeElapsed) + " seconds)")
+        elif selection == "4":
+            notWords = 0
+            startTime = time.time()
+            for i in range(len(aliceWords)): 
+                if binarySearch(dictionary, aliceWords[i].lower()) == -1: 
+                    endTime = time.time() 
+                    timeElapsed = endTime - startTime 
+                    notWords += 1 
+            print("Number of words not found in the dictionary: " + str(notWords) + " (" + str(timeElapsed) + " seconds)")
+        elif selection == "5":
+            loop = False
+
 # end main()
 
 
@@ -25,8 +83,26 @@ def loadWordsFromFile(fileName):
 
     # Split text by one or more whitespace characters
     return re.split('\s+', textData)
-# end loadWordsFromFile()
+# end loadWordsFromFile()    
+# Linear Search Function
+def linearSearch(anArray, item):
+    for i in range((len(anArray))):
+        if anArray[i] == item:
+            return i 
+    return -1 
+# Binary Search Function
+def binarySearch(anArray, item):
+    lowDex = 0
+    highDex = len(anArray) - 1
 
-
+    while lowDex <= highDex:
+        middleDex = math.floor((highDex + lowDex) / 2) 
+        if (item == str(anArray[middleDex])):
+            return middleDex 
+        elif (item < str(anArray[middleDex])):
+            highDex = middleDex - 1
+        else:
+            lowDex = middleDex + 1 
+    return -1 
 # Call main() to begin program
-main()
+main() 
